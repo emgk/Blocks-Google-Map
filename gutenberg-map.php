02f52126b498008f8cc63b618cb3878cc1e3c6bb
Govind Kumar <gkprmr@gmail.com>
2018-07-08 10:50:18 +0530

    💥💥 Added plugin's initial files.

---
+++ gutenberg-map.php
@@ -0,0 +1,12 @@
+<?php 
+/**
+ * Plugin Name: Gutenberg Map
+ * Description: Insert Map to your page through the gutenberg.
+ * Version: 1.0.0
+ * Author: Govind Kumar
+ * Author URI: http://emgk.github.io
+ * Text-Domain: gutenberg-map
+ */
+
+// Exit if not defined.
+defined('ABSPATH') || exit;
