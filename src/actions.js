69562924c55ff8bd212298f5f83d24592bf6a2d2
Govind Kumar <gkprmr@gmail.com>
2018-07-14 11:00:43 +0530

    import google map and placeautocomplet

--- src/actions.js
+++ src/actions.js
@@ -2,3 +2,10 @@ const { Component, Fragment } = wp.element;
 const { PanelBody } = wp.components;
 const { InspectorControls } = wp.editor;
 const { __ } = wp.i18n;
+
+import { GoogleMapContainer } from './google-map';
+import PlacesAutocomplete, {
+    geocodeByAddress,
+    getLatLng,
+} from 'react-places-autocomplete';
+
