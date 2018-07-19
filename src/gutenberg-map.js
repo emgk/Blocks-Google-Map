3a818b92d6d9a7faed023904ea7cf8d56ecc4b70
Govind Kumar <gkprmr@gmail.com>
2018-07-19 11:04:25 +0530

    registered gutenberg block

--- src/gutenberg-map.js
+++ src/gutenberg-map.js
@@ -3,3 +3,29 @@ const { registerBlockType } = wp.blocks;
 
 // Import actions.
 import { edit } from './actions';
+
+/**
+ * Register the gutenberg map block inside the embed category.
+ * 
+ * @since 1.0.0.
+ */
+registerBlockType('gutenberg-map-block/main-map-block', {
+    title: __('Map'),
+    icon: 'location',
+    category: 'embed',
+    attributes: {
+        latitute: {
+            type: "number",
+        },
+        longitude: {
+            type: "number"
+        },
+        tip: {
+            type: "string"
+        },
+        address: {
+            type: "string"
+        }
+    },
+    edit,
+});
