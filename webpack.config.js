befc3a01e906b482ca1d85afeb9f8977e71d6992
Govind Kumar <gkprmr@gmail.com>
2018-07-23 10:35:43 -0700

    Re-configure the webpack file.

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
 
-    entry: path.resolve(__dirname, './src/gutenberg-map.js'),
+    entry: path.resolve(__dirname, './src/blocks-google-maps.js'),
     output: {
-        filename: 'gutenberg-map.js',
+        filename: 'blocks-google-maps.js',
         path: path.resolve(__dirname, './assets/blocks')
     },
     devtool: 'source-map',
